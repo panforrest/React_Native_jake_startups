57e39b81f2db076aa54d258b2de6124191caea7d
panforrest <guoqianp@gmail.com>
2017-10-21 12:55:00 -0400

    4:44 render: This is This is the Startups Component

---
+++ components/Startups.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {
+  Text,
+  View
+} from 'react-native'
+
+class Startups extends Component {
+  render(){
+    return (
+      <View>
+        <Text>This is the Startups Component!</Text>
+      </View>
+    )
+  }
+}
+
+export default Startups
